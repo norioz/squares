3ddeb2953b2c13d97eab8a5e5e22c3d6144b22c8
Brett Groshong <norioz@gmail.com>
2013-02-15 14:18:10 -0800

    Testing the inclusion of phonegap in build.phonegap.

--- js/setup.js
+++ js/setup.js
@@ -1,20 +1,25 @@
+window.onresize = setupCanvas;
 function setupCanvas() {
     var width, height, canvas;
-    width = document.documentElement.clientWidth;
-    width = width > 600 ? 600 : width;
-    height = document.documentElement.clientHeight;
-    height = height > 600 ? 300 : height;
-    canvas = document.createElement('canvas');
-    canvas.id = "canvas1";
-    canvas.width = width;
-    canvas.height = height;
-    canvas.style.background = 'white';
-    canvas.style.zIndex = 10;
-    canvas.style.position = "absolute";
-    canvas.style.top = "0";
-    canvas.style.left ="0";
-    canvas.style.display = "none";
-    document.body.appendChild(canvas);
+    canvas = document.getElementById('canvas1');
+    if (canvas == null) {
+	canvas = document.createElement('canvas');
+	canvas.id = "canvas1";
+	canvas.style.background = 'white';
+	canvas.style.zIndex = 10;
+	canvas.style.position = "absolute";
+	canvas.style.top = "0";
+	canvas.style.left ="0";
+	canvas.style.display = "none";
+	document.body.appendChild(canvas);
+    }
+    if (typeof PhoneGap === 'undefined') {
+	canvas.width = 600;
+	canvas.height = 400;
+    } else {
+	canvas.width = document.documentElement.clientWidth;
+	canvas.height = document.documentElement.clientHeight;
+    }
 }
 function showCanvas() {
     document.getElementById("canvas1").style.display="inline";
